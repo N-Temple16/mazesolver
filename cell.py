8cf443e46886f1f85a327a54e9fd5e1f7c749925
N-Temple16 <nigelatemple@gmail.com>
2025-04-17 13:25:30 -0400

    added Cell draw_move method and Maze class

--- cell.py
+++ cell.py
@@ -29,3 +29,17 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+    
+    def draw_move(self, to_cell, undo=False):
+        x1 = (self._x1 + self._x2) // 2
+        y1 = (self._y1 + self._y2) // 2
+        x2 = (to_cell._x1 + to_cell._x2) // 2
+        y2 = (to_cell._y1 + to_cell._y2) // 2
+
+        if undo:
+            color = "gray" 
+        else:
+            color = "red"
+            
+        line = Line(Point(x1, y1), Point(x2, y2))
+        self._win.draw_line(line, color)
